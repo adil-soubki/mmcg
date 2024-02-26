ac0baf52d7053a5c1330581dcc3232aded405eba
Adil Soubki <5231841+adil-soubki@users.noreply.github.com>
2024-02-26 16:39:02 -0500

    use default mask length for audio model.
    * no longer need custom length with updated audio files.

--- src/models/multimodal_classifier.py
+++ src/models/multimodal_classifier.py
@@ -55,9 +55,6 @@ class MultimodalClassifier(torch.nn.Module):
             if config.audio_model_name_or_path
             else None
         )
-        # TODO: Only lower this when needed?
-        if self.audio_model:
-            self.audio_model.config.mask_time_length = 1  # XXX: Some clips are very short.
         # Throw if neither is given.
         if not self.text_model and not self.audio_model:
             raise ValueError("No text or audio model specified.")
